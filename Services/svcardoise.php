37f7feb154d905baca02cc916867283d976e0f54
marc <marc>
2006-06-19 17:00:21 +0000

    IE port Ardoise service

--- Services/svcardoise.php
+++ Services/svcardoise.php
@@ -5,7 +5,9 @@ function svcardoise(&$action) {
   $craie  = GetHttpVars("craie", "white");  
   $cline = GetHttpVars("cline", 5);
   $content = GetHttpVars("content", "");
+  if ($content!="") $action->parent->param->set("ARDOISE", $content, PARAM_USER.$action->user->id, $action->parent->id);
 
+  $content = getParam("ARDOISE");
   $action->lay->set("cline", $cline);
   $action->lay->set("craie", $craie);
   $action->lay->set("content", $content);
