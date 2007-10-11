6cb311cc9376090e637df2e75cd68ebc272cdced
marc <marc>
2007-10-11 05:23:31 +0000

    *** empty log message ***

--- Javascript/portal.js
+++ Javascript/portal.js
@@ -1,4 +1,4 @@
-// $Id: portal.js,v 1.44 2007/10/10 17:12:37 marc Exp $
+// $Id: portal.js,v 1.45 2007/10/11 05:23:31 marc Exp $
 
 // portal
 var portalRefreshInterval = 10;
@@ -146,7 +146,7 @@ function showService(is, updates) {
       imgcyc = '<img src="[IMGF:wd_svc_cyclic.gif:0,0,0|COLOR_BLACK]" style="border:0px" title="[TEXT:automatic reload all] '+services[is].rdel+' minutes">';
     }
     cnt += '<table cellspacing="0" cellpadding="0" style="width:100%; border:0px">';
-    cnt += '<tr style="vertical-align:baseline; cursor:e-resize; border:1px solid red;" onmousedown="return startMoveService(event, this, '+snum+');" onmouseup="endMoveService(event,'+snum+')"  onmouseover="mOverSvcTitle('+snum+')" onmouseout="mOutSvcTitle('+snum+')"onDblClick="showHideSvc(event, '+snum+',true); return false;">';
+    cnt += '<tr style="vertical-align:baseline; cursor:move; border:1px solid red;" onmousedown="return startMoveService(event, this, '+snum+');" onmouseup="endMoveService(event,'+snum+')"  onmouseover="mOverSvcTitle('+snum+')" onmouseout="mOutSvcTitle('+snum+')"onDblClick="showHideSvc(event, '+snum+',true); return false;">';
     cnt += '<td >';
      cnt += '<img id="ivsvc'+snum+'" style="margin-left:2px" class="small_button" onclick="showHideSvc(event, '+snum+',true); return false;" src="[IMGF:wd_svc_hide.gif:0,0,0|COLOR_BLACK]" title="[TEXT:wd hide svc content]">';
     cnt += '<span id="tsvcti'+snum+'">'+stitle+'</span> '+imgcyc+'</td>';
@@ -794,25 +794,71 @@ function unsetTempoCloseSubService(event, mid) {
 // Resizing columns
 var colResize = -1;
 var colElt = null;
+var pWidth = 0; 
+var cWidth = new Array();
+var cLeft = new Array();
+function initResizePortalCol(event, ncol) {
+  event || (event = window.event);
+  var srcel = (event.target) ? event.target : event.srcElement;
+  if (colResize==-1) {
+    srcel.style.borderRight = '3px outset [COLOR_A8]';
+    globalcursor('ew-resize');
+    trace('sélectionnée ==> '+ncol);
+  }
+  return false;
+}
+
 function startResizePortalCol(event, ncol) {
   event || (event = window.event);
   var srcel = (event.target) ? event.target : event.srcElement;
 
   srcel.style.borderRight = '3px outset [COLOR_A8]';
-  colElt = srcel;
+  globalcursor('ew-resize');
+
+  colResize = ncol;
+  colElt = document.getElementById('wdcol'+ncol);
+
+  pWidth = getFrameWidth();
+
+  for (var icol=0; icol<colCount; icol++) {
+    cWidth[icol] = getObjectWidth(document.getElementById('wdcol'+icol));
+    cLeft[icol]  = getAnchorPosition('wdcol'+icol).x;
+  }
+
   addEvent(document,'mousemove',mouseResizeCols);  
   addEvent(document,'mouseup',endResizeCols);  
   stopPropagation(event);
-  colResize = ncol;
+
   return;
 }
 
 function mouseResizeCols(event) {
+  var tw = 1.0;
+  var pc = 0.0;
   if (colResize>=0) {
     GetXY(event);
-    colElt.style.left = Xpos;
-    document.getElementById('wdcol'+colResize).style.width = parseInt(Xpos);
-    trace(' X='+Xpos);
+    if (cLeft[colResize]<Xpos && Xpos<pWidth) {
+      var dv = Xpos - (cLeft[colResize]+cWidth[colResize]);
+      var nw = new Array();
+      nw[colResize] = parseInt(100 / (pWidth / (cWidth[colResize] + dv)));
+      tw = pWidth - (cWidth[colResize] + dv);
+      var mmm = ' tw='+tw+'   : ';
+      for (var icol=0; icol<colCount; icol++) {
+	pc =  parseInt(100 / (pWidth / cWidth[icol]));
+        if (icol!=colResize) {
+	  nw[icol] = parseInt((100 / (pWidth / (tw * (pc/100) )))/100);
+	}
+	mmm += ' col['+icol+']='+nw[icol]+'% (pc:'+pc+')';
+      }
+      trace(mmm);
+
+
+
+      for (var icol=0; icol<colCount; icol++) {
+// 	document.getElementById('wdcol'+colResize).style.width = nw[icol]+'%';
+// 	cWidth[icol] = getObjectWidth(document.getElementById('wdcol'+icol));
+      }
+    }
   }
   return false;
 }
@@ -821,19 +867,25 @@ function endResizeCols(event) {
   event || (event = window.event);
   var srcel = (event.target) ? event.target : event.srcElement;
   if (colResize>=0) {
-    GetXY(event);
-    trace('ca bouge : X='+Xpos+' Y='+Ypos);
     srcel.style.borderRight = '0px';
+    unglobalcursor();
     colElt = null;
     colResize = -1;
     delEvent(document,'mouseup',endResizeCols);
+    var mmm = 'Store page=1 : ';
+    for (var icol=0; icol<colCount; icol++) {
+      mmm += ' col['+icol+'] ='+parseInt(100/(pWidth/cWidth[icol]))+'% ';
+    }
+    trace(mmm);
  }
   return;
 }
 function endResizePortalCol(event, ncol) {
   event || (event = window.event);
   var srcel = (event.target) ? event.target : event.srcElement;
-
-  //  alert('resizing col #'+ncol);
-
+  if (colResize==-1) {
+    srcel.style.borderRight = '0px';
+    unglobalcursor();
+  }
+  return false;
 }
