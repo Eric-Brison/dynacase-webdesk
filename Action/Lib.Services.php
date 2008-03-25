93a72b7b025ff8b29575b0d1033e8b41d39e4b2f
eric <eric>
2008-03-25 17:34:05 +0000

    Correct for passwords with /

--- Action/Lib.Services.php
+++ Action/Lib.Services.php
@@ -3,8 +3,8 @@
 function parseUrl($link) {
 
   global $_SERVER;
-  $gparams = array( "[user]" => $_SERVER['PHP_AUTH_USER'], 
-		    "[pass]" => $_SERVER['PHP_AUTH_PW'],
+  $gparams = array( "[user]" => urlencode($_SERVER['PHP_AUTH_USER']), 
+		    "[pass]" => urlencode($_SERVER['PHP_AUTH_PW']),
 		    );
   $ms = $mr = array();
   foreach ($gparams as $k => $v) {
